f548555a0ba9afc7555cdbc9986acafdf85ad42d
Kiggles <ridgewwoods@gmail.com>
2023-05-04 18:00:47 -0400

    fixed title and entry field in post.js

--- public/js/post.js
+++ public/js/post.js
@@ -1,8 +1,8 @@
 const newPostHandler = async (event) => {
   event.preventDefault();
 console.log("here");
-  const title = document.querySelector('#new-post-entry-field').value.trim();
-  const body = document.querySelector('#new-post-title-field').value.trim();
+  const title = document.querySelector('#new-post-title-field').value.trim();
+  const body = document.querySelector('#new-post-entry-field').value.trim();
   const created_at = "May 2, 2023 09:30:00";
 
   if (title && body && created_at) {
