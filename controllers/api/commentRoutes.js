0d0c4e52e9f39dc2552478224f6f810becf54327
Kiggles <ridgewwoods@gmail.com>
2023-05-04 15:23:05 -0400

    hopefully didnt break anything

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,19 +1,38 @@
 const router = require('express').Router();
-const { forumPost, Comment } = require('../../models');
+const { ForumPost, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
-  router.post('/:id', withAuth,  async (req, res) => {
-    try {
-      const newComment = await Comment.create({
-        ...req.body,
-        user_id: req.session.user_id,
-      });
+router.post('/:id',  async (req, res) => {
+  try {
+    // const newComment = await Comment.create({
+    //   ...req.body,
+    //   user_id: req.session.user_id,
+    // });
+
+    const post = await ForumPost.findOne({
+      where: {
+          id: req.params.id
+      }
+  })
+
+  const postId = post.id
+  // const postId = req.params.post_id
+
+  const newComment = await Comment.create({
+    
+      body: req.body.content, 
+      user_id: "User_id", 
+      created_at: "5"
+  })
   
-      res.status(200).json(newComment);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+    console.log(err)
+    console.log(req.body)
+  }
+});
 
   module.exports = router;
--- controllers/api/gameRoutes.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-const { ForumPost } = require('../../models');
-
-
-  module.exports = router;
