7a2d8acc280f85055f81ebdfae9f09309623fab2
Kiggles <ridgewwoods@gmail.com>
2023-05-04 18:06:50 -0400

    fixed date on post

--- public/js/post.js
+++ public/js/post.js
@@ -3,7 +3,7 @@ const newPostHandler = async (event) => {
 console.log("here");
   const title = document.querySelector('#new-post-title-field').value.trim();
   const body = document.querySelector('#new-post-entry-field').value.trim();
-  const created_at = "May 2, 2023 09:30:00";
+  const created_at = new Date();
 
   if (title && body && created_at) {
     const response = await fetch('/api/posts', {
